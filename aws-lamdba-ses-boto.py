e3885b4ae804c08aa2aee9c97526019b8e1ecd3e
Faisal Younus <gitfyhub>
2020-02-19 16:12:47 -0600

    initial

---
+++ aws-lamdba-ses-boto.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    
+    client = boto3.client('ses')
+    SUBJECT = "Amazon SES Message"
+    BODY_TEXT = ("Amazon SES (Python)\r\n"
+             "This email was sent with Amazon SES using the "
+             "AWS SDK for Python (Boto)."
+            )
+            
+    message = {"Subject": { "Data": SUBJECT} , "Body": {"Html": {"Data": BODY_TEXT}}}
+    response = client.send_email(Source = "xxxx@gmail.com", Destination = {"ToAddresses":["xxxx@gmail.com"]}, Message = message)
+    
+    return {
+        'statusCode': 200,
+        'body': response
+    }
